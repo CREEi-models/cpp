626c5e1fb7cc9651a07ffb885f8df9dcd416177d
francoislauger <francoislauger@gmail.com>
2020-01-13 12:03:00 -0500

    add a integration test

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,33 +1,32 @@
 import cpp
 import pytest
-import warnings
+
 @pytest.fixture
 def rules():
     rules = cpp.rules(qpp=True)
     return rules
 
-def pending():
-    warnings.warn(PendingDeprecationWarning("This method will be removed in future versions.  Use 'tree.iter()' or 'list(tree.iter())' instead. for elem in self.tree.iter() if Element_has_iter else self.tree.getiterator():"))
-    return 1
-##@pytest.fixture
-#def
 ympe_value = [(1966, 5000),
               (1980, 13100),
               (2025, 68539),
-              (2060, 192858)]
-@pytest.mark.filterwarnings("ignore:PendingDeprecationWarning")
-class TestCPP:
+              (2060, 177549.94)]
+benefit_value = [(64,64,1,2020,1092.54),
+                (65,65,1,2020,1177.30),
+                (66,66,1,2020,1276.20)]
 
-    #def test_load_rules(self):
-    #    yrspars =cpp.load_rules()
-     #   assert type(yrspars) == cpp.np.recarray
-
-    def test_init_rules(self,rules):       
+class Test_unittest:
+    def test_init_rules(self,rules):  
         assert isinstance(rules,cpp.rules)
-    
-    #def test_rules_loc(self,rules): 
-    #    assert  rules.loc(2010) == 44
-    
-    @pytest.mark.parametrize( "year, value",ympe_value)                           
-    def test_rules_ympe(self, rules, year,value):
+
+    @pytest.mark.parametrize( "year, value",ympe_value)  
+    def test_rules_ympe(self,rules,year,value):
         assert  rules.ympe(year) == pytest.approx(value,0.5)
+
+class Test_integration:
+    @pytest.mark.parametrize("claimage, retage, ratio, year, benefit",benefit_value)  
+    def test_benefit(self,rules,claimage,retage,ratio,year,benefit):
+        case = cpp.account(year-claimage,rules=rules)
+        case.SetHistory_ratio(retage=retage,a={"r":1.1,'n':retage-18})
+        case.SetHistory_fam(claimage=claimage,age_birth=[])
+        case.RunCase(claimage=claimage)
+        assert case.gBenefit(year) == pytest.approx(benefit,5)
