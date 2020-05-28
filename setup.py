fcc4e21f1e624a92833930b4cafb04e878d44515
Pierre-Carl <pcmichaud@gmail.com>
2020-05-28 11:25:39 -0400

    changes to setup

--- setup.py
+++ setup.py
@@ -19,5 +19,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+   'pandas',
+   'numpy'
+    ],
     python_requires='>=3.6',
 )
