88e704ef7d7fec49df484f1d2a098f6a7974415a
Pierre-Carl <pcmichaud@gmail.com>
2020-05-28 11:20:09 -0400

    changes to setup

--- setup.py
+++ setup.py
@@ -11,17 +11,13 @@ setuptools.setup(
     description="Module to simulate CPP and QPP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rsi-models/cpp",
+    url="https://github.com/rsi-models/srp",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=[
-        "numpy",
-        "pandas",
-        "xlrd"
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: To BE DETERMINE",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    python_requires='>=3.6',
 )
