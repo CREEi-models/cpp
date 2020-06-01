b120ecb96865f4195cbf2d71773911226c5ff234
Pierre-Carl <pcmichaud@gmail.com>
2020-06-01 09:42:18 -0400

    mistake in test

--- tests/unittest/test_unittest.py
+++ tests/unittest/test_unittest.py
@@ -16,7 +16,7 @@ benefit_value = [(64,64,1,2020,1092.54),
 
 class Test_unittest:
     def test_init_rules(self,rules):  
-        assert isinstance(rules,srp.rules)
+        assert isinstance(rules,srpp.rules)
 
     @pytest.mark.parametrize( "year, value",ympe_value)  
     def test_rules_ympe(self,rules,year,value):
@@ -25,7 +25,7 @@ class Test_unittest:
 class Test_integration:
     @pytest.mark.parametrize("claimage, retage, ratio, year, benefit",benefit_value)  
     def test_benefit(self,rules,claimage,retage,ratio,year,benefit):
-        case = srp.account(year-claimage,rules=rules)
+        case = srpp.account(year-claimage,rules=rules)
         case.SetHistory_ratio(retage=retage,a={"r":1.1,'n':retage-18})
         case.SetHistory_fam(claimage=claimage,age_birth=[])
         case.RunCase(claimage=claimage)
