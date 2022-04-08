e63891acbf9e83d3e9babe1f503ff6a561e6fd38
Nicholas-James Clavet <nicholas-james.clavet@hec.ca>
2022-04-08 16:11:17 -0400

    Update setup and readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# srpp
+# SRPP
 Simulateur de régimes de pensions publiques (SRPP) au Canada. 
 La documentation est disponible [ici](https://creei-models.github.io/srpp/index.html).
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="srpp",
-    version="0.6.0",
+    version="0.6.3",
     author="Francois Laliberte-Auger, Nicholas-James Clavet, Pierre-Carl Michaud",
     author_email="francois.laliberte-auger@hec.ca",
     description="Module pour simuler prestations pensions publiques Canada",
